acab47873992a929653f229a6db59372c8e21cea
nasua <acollazocacho@gmail.com>
2024-05-14 14:59:32 +0200

    refactor

--- Ejercicios/ejercicio2Mio.js
+++ Ejercicios/ejercicio2Mio.js
@@ -12,7 +12,7 @@ const datosPizza = [
     { nombre: "Pizza atún", precio: 22, identificador: 3, ingredientes: ["Atún", "Aceitunas negras", "Anchoa"] }
 ];
 
-const idIntroducido = prompt("Introduce el id de cliente");
+const idIntroducido = parseInt(prompt("Introduce el id de cliente"));
 const clienteEncontrado = datosClientes.find(cliente=>cliente.id==idIntroducido);
 const pizzaEncontrada =  datosPizza.find(pizza=>pizza.identificador==idIntroducido);
 const divCliente = document.getElementById("cliente");
@@ -22,7 +22,7 @@ let pizza = null;
 document.addEventListener("DOMContentLoaded",ev => {
 
 //Apartado 1
-const mostrarDatosCliente = (idIntroducido) =>{
+const mostrarDatosCliente = () =>{
 
     if(clienteEncontrado){
       const nombre = clienteEncontrado.first_name +" "+clienteEncontrado.last_name;
@@ -34,21 +34,16 @@ const mostrarDatosCliente = (idIntroducido) =>{
         alert("Cliente no encontrado");
     }
 }
-mostrarDatosCliente(idIntroducido);
+mostrarDatosCliente();
 
 //Apartado 3
-const mostrarIngredientesPizza = (idIntroducido)=>{
+const mostrarIngredientesPizza = ()=>{
     if(pizzaEncontrada){
         const nombre =   pizzaEncontrada.nombre;
         const ingredientes = pizzaEncontrada.ingredientes;
         const linea= document.createElement("h1");
         linea.innerHTML= "Los ingredientes de la "+nombre+" son:";
         divIngredientes.appendChild(linea);
-        // for (let i = 0; i < pizzaEncontrada.ingredientes.length; i++) {
-        //     const parrafo = document.createElement("h2");
-        //     parrafo.innerHTML=ingredientes[i];
-        //     divIngredientes.appendChild(parrafo);
-        // }
         ingredientes.forEach(ingrediente => {
             const parrafo = document.createElement("h2");
             parrafo.innerHTML = ingrediente;
@@ -58,10 +53,11 @@ const mostrarIngredientesPizza = (idIntroducido)=>{
     else{
         alert("Pizza no encontrada");
     }
+    crearBotonComprar();
 }
 //Apartado 7: Al cargar los ingredientes de la pizza que el resultado se muestre transcurrido unos segundos.
 setTimeout(()=>{
-    mostrarIngredientesPizza(idIntroducido);
+    mostrarIngredientesPizza();
 },2000);
 
 
@@ -71,10 +67,11 @@ const mostrarTablaDatosPizza = ()=>{
     const table = document.createElement("table");
     const encabezado = document.createElement("tr");
     const thNombre = document.createElement("th");
-    const thPrecio = document.createElement("th");thNombre.textContent="NOMBRE";
+    const thPrecio = document.createElement("th");;
+    thNombre.textContent="NOMBRE"
+    thPrecio.textContent="PRECIO";
 
     encabezado.appendChild(thNombre);
-    thPrecio.textContent="PRECIO";
     encabezado.appendChild(thPrecio);
     table.appendChild(encabezado);
 
@@ -102,20 +99,12 @@ const crearBotonComprar=()=>{
         comprarPizza(pizzaEncontrada);
     });
 }
-crearBotonComprar();
+
 //Apartado 5
 // Función para almacenar en el LocalStorage al hacer clic en el botón Comprar
-const comprarPizza = (pizzaEncontrada) =>{
-    let { identificador } = pizza;
-    localStorage.setItem(identificador,JSON.stringify(pizzaEncontrada));
+const comprarPizza = () =>{
+    localStorage.setItem(idIntroducido,JSON.stringify(pizzaEncontrada));
     console.log("pizza almacenada en carrito");
 }
-
 //Apartado 6 : Utilizar find, funciones flecha, o/y map, declaración correcta de variables, etc.
-
 })
-
-
-
-
-
