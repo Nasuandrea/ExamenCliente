077f5018a7db0b0839a3ca04a7d37c310eafbc74
nasua <acollazocacho@gmail.com>
2024-05-14 13:38:37 +0200

    refactor name

--- Ejercicios/ejercicio2Mio.js
+++ Ejercicios/ejercicio2Mio.js
@@ -1,69 +1,32 @@
 //Declarar un array de objetos de clientes
-var datosClientes = [{
-    id: 1,
-    email: "michael.lawson@reqres.in",
-    first_name: "Michael",
-    last_name: "Lawson"
-},
-    {
-        id: 2,
-        email: "lindsay.ferguson@reqres.in",
-        first_name: "Lindsay", "last_name": "Ferguson"
-    }
-    , {
-        id: 3,
-        email: "tobias.funke@reqres.in",
-        first_name: "Tobias",
-        last_name: "Funke"
-    },
-    {
-        id: 4,
-        email: "byron.fields@reqres.in",
-        first_name: "Byron",
-        last_name: "Fields"
-    }];
-//Declara un array de objetos de pizzas
-var datosPizza = [
-    {
-        nombre: "Pizza margarita",
-        precio: 20,
-        identificador: 1,
-        ingredientes: [
-            "Champiñones",
-            "Jamón Cocido"
-        ]
-    },
-    {
-        nombre: "Pizza barbacoa",
-        precio: 25,
-        identificador: 2,
-        ingredientes: [
-            "Carne",
-            "Salsa barbacoa",
-            "Extra de queso"
-        ]
-    },
-    {
-        nombre: "Pizza atún",
-        precio: 22,
-        identificador: 3,
-        ingredientes: [
-            "Atún",
-            "Aceitunas negras",
-            "Anchoa"
-        ]
-    }
+const datosClientes = [
+    { id: 1, email: "michael.lawson@reqres.in", first_name: "Michael", last_name: "Lawson" },
+    { id: 2, email: "lindsay.ferguson@reqres.in", first_name: "Lindsay", last_name: "Ferguson" },
+    { id: 3, email: "tobias.funke@reqres.in", first_name: "Tobias", last_name: "Funke" },
+    { id: 4, email: "byron.fields@reqres.in", first_name: "Byron", last_name: "Fields" }
+];
+// Array de objetos pizzas
+const datosPizza = [
+    { nombre: "Pizza margarita", precio: 20, identificador: 1, ingredientes: ["Champiñones", "Jamón Cocido"] },
+    { nombre: "Pizza barbacoa", precio: 25, identificador: 2, ingredientes: ["Carne", "Salsa barbacoa", "Extra de queso"] },
+    { nombre: "Pizza atún", precio: 22, identificador: 3, ingredientes: ["Atún", "Aceitunas negras", "Anchoa"] }
 ];
 
-let idIntroducido = prompt("Introduce el id de cliente");
+const idIntroducido = prompt("Introduce el id de cliente");
+const clienteEncontrado = datosClientes.find(cliente=>cliente.id==idIntroducido);
+const pizzaEncontrada =  datosPizza.find(pizza=>pizza.identificador==idIntroducido);
+const divCliente = document.getElementById("cliente");
+const divIngredientes = document.getElementById("ingredientes");
+const divPizza = document.getElementById("pizzas");
+let pizza = null;
+document.addEventListener("DOMContentLoaded",ev => {
 
 //Apartado 1
 const mostrarDatosCliente = (idIntroducido) =>{
-    let clienteEncontrado = datosClientes.find(cliente=>cliente.id==idIntroducido);
+
     if(clienteEncontrado){
-      let nombre =   clienteEncontrado.first_name +" "+clienteEncontrado.last_name;
-      let divCliente = document.getElementById("cliente");
-      let linea= document.createElement("h1");
+      const nombre = clienteEncontrado.first_name +" "+clienteEncontrado.last_name;
+      const linea= document.createElement("h1");
       linea.innerHTML= nombre;
       divCliente.appendChild(linea);
     }
@@ -75,22 +38,25 @@ mostrarDatosCliente(idIntroducido);
 
 //Apartado 3
 const mostrarIngredientesPizza = (idIntroducido)=>{
-    let pizzaEncontrada =  datosPizza.find(pizza=>pizza.identificador==idIntroducido);
     if(pizzaEncontrada){
-        let nombre =   pizzaEncontrada.nombre;
-        let ingredientes = pizzaEncontrada.ingredientes;
-        let divIngredientes = document.getElementById("ingredientes");
-        let linea= document.createElement("h1");
+        const nombre =   pizzaEncontrada.nombre;
+        const ingredientes = pizzaEncontrada.ingredientes;
+        const linea= document.createElement("h1");
         linea.innerHTML= "Los ingredientes de la "+nombre+" son:";
         divIngredientes.appendChild(linea);
-        for (let i = 0; i < pizzaEncontrada.ingredientes.length; i++) {
-            let parrafo = document.createElement("h2");
-            parrafo.innerHTML=ingredientes[i];
+        // for (let i = 0; i < pizzaEncontrada.ingredientes.length; i++) {
+        //     const parrafo = document.createElement("h2");
+        //     parrafo.innerHTML=ingredientes[i];
+        //     divIngredientes.appendChild(parrafo);
+        // }
+        ingredientes.forEach(ingrediente => {
+            const parrafo = document.createElement("h2");
+            parrafo.innerHTML = ingrediente;
             divIngredientes.appendChild(parrafo);
-        }
+        });
     }
     else{
-        alert("Cliente no encontrado");
+        alert("Pizza no encontrada");
     }
 }
 //Apartado 7: Al cargar los ingredientes de la pizza que el resultado se muestre transcurrido unos segundos.
@@ -101,22 +67,22 @@ setTimeout(()=>{
 
 //Apartado 2
 const mostrarTablaDatosPizza = ()=>{
-    let divPizza = document.getElementById("pizzas");
-    let table = document.createElement("table");
-    let encabezado = document.createElement("tr");
-    let thNombre = document.createElement("th");
-    thNombre.textContent="NOMBRE";
+
+    const table = document.createElement("table");
+    const encabezado = document.createElement("tr");
+    const thNombre = document.createElement("th");
+    const thPrecio = document.createElement("th");thNombre.textContent="NOMBRE";
+
     encabezado.appendChild(thNombre);
-    let thPrecio = document.createElement("th");
     thPrecio.textContent="PRECIO";
     encabezado.appendChild(thPrecio);
     table.appendChild(encabezado);
-    // table.appendChild(th);
+
     datosPizza.forEach(pizza =>{
-        let tr = document.createElement("tr");
-        let tdNombre = document.createElement("td");
+        const tr = document.createElement("tr");
+        const tdNombre = document.createElement("td");
         tdNombre.textContent= pizza.nombre;
-        let tdPrecio = document.createElement("td");
+        const tdPrecio = document.createElement("td");
         tdPrecio.textContent = pizza.precio + "€";
         tr.appendChild(tdNombre);
         tr.appendChild(tdPrecio);
@@ -126,15 +92,12 @@ const mostrarTablaDatosPizza = ()=>{
 }
 mostrarTablaDatosPizza();
 
-
 //Apartado 4
 const crearBotonComprar=()=>{
-    let boton = document.createElement("button");
-    let divIngredientes = document.getElementById("ingredientes");
+    const boton = document.createElement("button");
     boton.textContent = "Comprar";
     divIngredientes.appendChild(boton);
     //Apartado 5
-    let pizzaEncontrada =  datosPizza.find(pizza=>pizza.identificador==idIntroducido);
     boton.addEventListener("click",() =>{
         comprarPizza(pizzaEncontrada);
     });
@@ -143,13 +106,14 @@ crearBotonComprar();
 //Apartado 5
 // Función para almacenar en el LocalStorage al hacer clic en el botón Comprar
 const comprarPizza = (pizzaEncontrada) =>{
-    localStorage.setItem("pizzaEncontrada",JSON.stringify(pizzaEncontrada));
+    let { identificador } = pizza;
+    localStorage.setItem(identificador,JSON.stringify(pizzaEncontrada));
     console.log("pizza almacenada en carrito");
 }
 
 //Apartado 6 : Utilizar find, funciones flecha, o/y map, declaración correcta de variables, etc.
 
-
+})
 
 
 
